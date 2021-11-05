c6850d8f6d6b629e3d01265951c3e9d0ce7cfbe9
yf-steven <music.beats.spirit@gmail.com>
2021-11-06 08:19:38 +0900

    DBUtilファイルの修正

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -4,19 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import constants.JpaConst;
+
 public class DBUtil {
 
     private static EntityManagerFactory emf;
 
-    private static final String PERSISTENCE_UNIT_NAME = "live_schedule";
-
+    // EntityManagerインスタンスを生成
     public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
     }
 
+    // EntityManagetFactoryインスタンスを生成
     private static EntityManagerFactory _getEntityManagerFactory() {
         if (emf == null) {
-            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
         }
 
         return emf;
